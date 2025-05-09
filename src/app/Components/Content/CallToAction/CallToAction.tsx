99338a22e0cd49b60ca4c0418c8debe73de67da1
Victoria Hawley <v.l.hawley@hotmail.com>
2025-05-09 14:26:29 +0100

    desktop hero spacing

--- src/app/Components/Content/CallToAction/CallToAction.tsx
+++ src/app/Components/Content/CallToAction/CallToAction.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material";
 import { MainBtn } from "../../Buttons/MainButton/MainBtn";
-import image from  './../../../assets/bannerImage.jpg';
+import image from  './../../../assets/Stanley 5 years.png';
 
 interface iCallToAction{
     title: string;
@@ -16,11 +16,11 @@ export function CallToAction({title, description, btnTitle, titleColor, descript
 
     console.log('what is the image import?', image)
     return(
-        <Stack justifyContent={'center'} alignItems={'center'}  flexDirection='row' gap={4} style={{backgroundColor: backgroundColor, borderRadius: borderRadius, padding: 12}}>
-            <Stack  alignItems='flex-start' flexDirection='column' justifyContent={'flex-start'} gap={3} style={{width: '60%'}}>
-                <h1 style={{fontSize: 58, fontWeight: 800, width: '70%', color: titleColor}}>{title}</h1>
+        <Stack justifyContent={'center'} alignItems={'center'}  flexDirection='row' gap={4} style={{backgroundColor: backgroundColor, borderRadius: borderRadius}}>
+            <Stack  alignItems='flex-start' flexDirection='column' justifyContent={'flex-start'} style={{width: '60%'}}>
+                <h1 style={{fontSize: 56, letterSpacing: -1, textWrap: 'balance',lineHeight: 1.14, fontWeight: 800, width: '70%', color: titleColor}}>{title}</h1>
                 <p style={{width: '80%', fontSize: 24, fontWeight: 500, color: descriptionColor}}>{description}</p>
-                <MainBtn title={btnTitle} type='cta'/>
+                <div style={{marginTop: 24}}><MainBtn title={btnTitle} type='cta'/></div>
             </Stack>
             <Stack justifyContent='flex-start'  alignItems='center' >
                 <img src={image.src} style={{borderRadius: '100%', alignSelf: 'flex-start'}} alt='people communicating over a computer' height={350} width={350}/>
--- src/app/Components/Hero/Hero.tsx
+++ src/app/Components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import { CallToAction } from "../Content/CallToAction/CallToAction";
 
 export function Hero(){
     return(
-        <div style={{justifyContent: 'center', alignItems: 'center', backgroundColor: '#F41B5D'}}>
+        <div style={{justifyContent: 'center', alignItems: 'center', backgroundColor: '#F41B5D', paddingTop: '5%', paddingBottom: '5%', paddingRight: '10%', paddingLeft: '10%'}}>
             <CallToAction title='Live longer with Vitality insurance' 
             description="Get covered. Get healthier. And get this - you could live up to 5 years longer when you get active with Vitality"
             btnTitle="Get a quote in minutes"/>
